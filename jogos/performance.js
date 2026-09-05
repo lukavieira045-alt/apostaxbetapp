682ec4155864d31d2bcbf36bca77a9013039ce74
lukavieira045-alt <lukavieira045@gmail.com>
2026-09-05 07:41:10 -0300

    Restaurar controlador neutro da Roleta 2

---
+++ jogos/performance.js
@@ -0,0 +1,11 @@
+/*
+ * Roleta 2
+ *
+ * Este arquivo fica propositalmente sem controlador de animação.
+ * A animação da roda e da bola pertence exclusivamente ao girarRoleta()
+ * dentro de roleta2.html. Isso evita dois controladores disputando o
+ * mesmo transform e causando giro incorreto/salto da bola no final.
+ */
+(function () {
+  'use strict';
+})();
